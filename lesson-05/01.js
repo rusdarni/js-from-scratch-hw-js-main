58ae67ecd1f378fb33728ea9a1c5a425790d7370
dasha <rus.dar.ni@gmail.com>
2025-11-27 23:00:46 +0300

    lesson-05-01-00

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -11,9 +11,17 @@
 */
 
 const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource() {}
+    resources: {
+        gold: 250,
+        lumber: 100,
+    },
+    addResource(resource, amount) {
+        for (const key in game.resources) {
+            if (resource !== key) {
+                console.log("Invalid resource")
+            } else {
+                game.resources[key] += amount;
+            }
+        }
+    }
 }

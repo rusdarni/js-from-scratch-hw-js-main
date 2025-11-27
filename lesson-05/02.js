a7e1d8b94d6f53bdada1f01cbed8a044b16c8451
dasha <rus.dar.ni@gmail.com>
2025-11-27 23:30:27 +0300

    lesson-05-02-00

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,8 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(object, pictureName, newName) {
+      object[pictureName] = newName
+}
+
+

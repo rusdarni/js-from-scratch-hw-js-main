1dc059ee667366a20063e977807de615df4b38f0
dasha <rus.dar.ni@gmail.com>
2025-12-23 01:07:40 +0300

    lesson-07-01-00

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,23 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const changeImageButtonPrev = document.querySelector('#prev-button')
+const changeImageButtonNext = document.querySelector('#next-button')
+let index = 0
+changeImageButtonPrev.addEventListener('click', function() {
+  const images = document.querySelector('#web-tech-image')
+  index = index - 1
+  if(index<0){
+    index = index+3
+  }
+  images.setAttribute('src', WEB_TECH_IMAGES[index])
+})
+changeImageButtonNext.addEventListener('click', function() {
+  const images = document.querySelector('#web-tech-image')
+  index = index + 1
+  if (index>2){
+    index = index-3
+  }
+  images.setAttribute('src', WEB_TECH_IMAGES[index])
+})
